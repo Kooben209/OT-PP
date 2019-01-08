c559e06f200c5a4569295a63346bc7850ded952c
Kooben209 <43027402+Kooben209@users.noreply.github.com>
2019-01-08 19:07:09 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -115,8 +115,8 @@ with requests.session() as s:
 					nextPageURL = checkURL+"?page="+str(page-1)					
 				else:
 					nextPageURL = checkURL+"&page="+str(page-1)
-					
-				print('requesting next page')
+				if os.environ.get('MORPH_DEBUG') == "1":	
+					print('requesting next page')
 				if os.environ.get('MORPH_DEBUG') == "1":
 					print(nextPageURL)
 				r1 = s.get(nextPageURL)
